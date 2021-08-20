0ae244b75dfafbd542a8edb838fbfa84df05f98b
yxy <charlay_yu@163.com>
2021-08-20 21:53:33 +0800

    insert

--- src/main/java/yxy/mapper/UserMapper.java
+++ src/main/java/yxy/mapper/UserMapper.java
@@ -9,4 +9,5 @@ public interface UserMapper {
     SysUser selectById(Long id);
     List<SysUser> selectAll();
     List<SysRole> selectRolesByUserId(Long userId);
+    int insert(SysUser sysUser);
 }
--- src/test/java/yxy/mapper/UserMapperTest.java
+++ src/test/java/yxy/mapper/UserMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import yxy.model.SysRole;
 import yxy.model.SysUser;
 
+import java.util.Date;
 import java.util.List;
 
 public class UserMapperTest extends BaseMapperTest{
@@ -50,4 +51,26 @@ public class UserMapperTest extends BaseMapperTest{
         }
     }
 
+
+    @Test
+    public void testInsert(){
+        SqlSession sqlSession = getSqlSession();
+        try{
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+
+            SysUser user = new SysUser();
+            user.setUserName("test1");
+            user.setUserPassword("1234321");
+            user.setUserEmail("test@test.com");
+            user.setUserInfo("test info");
+            user.setHeadImg(new byte[]{1,2,3,4});
+            user.setCreateTime(new Date());
+            int result = userMapper.insert(user);
+            Assert.assertEquals(1, result);
+            Assert.assertNull(user.getId());
+        }finally {
+            sqlSession.rollback();
+            sqlSession.close();
+        }
+    }
 }
